4c3b8fafcc4e92f9f9bced53fe5494a4e12cc1b8
Devic Ognjen <jsguru@JSs-MacBook-Pro.local>
2022-08-17 15:18:36 +0200

    changes

--- src/routes/path-constants.ts
+++ src/routes/path-constants.ts
@@ -1,6 +1,8 @@
-export const HOME_PATH = '/';
-export const CONTACT_PATH = '/kontakt';
-export const O_NAMA_PATH = '/o-nama';
-export const ADMIN_PANEL = '/admin';
-export const ADMIN_PANEL_SCREEN = '/admin-panel';
+/* eslint-disable operator-linebreak */
+export const HOME_PATH = '/http://DevaDeveloper.github.io/sk-kozara';
+export const CONTACT_PATH = 'http://DevaDeveloper.github.io/sk-kozara/kontakt';
+export const O_NAMA_PATH = 'http://DevaDeveloper.github.io/sk-kozara/o-nama';
+export const ADMIN_PANEL = 'http://DevaDeveloper.github.io/sk-kozara/admin';
+export const ADMIN_PANEL_SCREEN =
+  'http://DevaDeveloper.github.io/sk-kozara/admin-panel';
 // export const ONE_POST = '/objava';
